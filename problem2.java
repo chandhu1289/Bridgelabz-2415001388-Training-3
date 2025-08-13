f6e8375e03eff10ba8b8b8d8dbd25de5d9a947fd
Chandu <chandhuchandhu1289@gmail.com>
2025-08-13 15:00:53 +0530

    First commit

---
+++ problem2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class problem2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter distance in kilometers: ");
+        double kilometers = scanner.nextDouble();
+
+        double miles = kilometers * 0.621371;
+
+        System.out.printf("%.2f kilometers is equal to %.2f miles\n", kilometers, miles);
+
+        scanner.close();
+    }
+}
